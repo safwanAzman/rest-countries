94d557b08cb3ffda3fa764481bf38056c3cf44b1
safwan <safwanazman017@gmail.com>
2023-02-08 10:12:42 +0800

    add env

--- next.config.js
+++ next.config.js
@@ -3,4 +3,45 @@ const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = nextConfig
+
+
+const {
+  PHASE_DEVELOPMENT_SERVER,
+  PHASE_PRODUCTION_BUILD,
+} = require('next/constants')
+
+// This uses phases as outlined here: https://nextjs.org/docs/#custom-configuration
+module.exports = (phase) => {
+  // nextConfig
+  // when started in development mode `next dev` or `npm run dev` regardless of the value of STAGING environment variable
+  const isDev = phase === PHASE_DEVELOPMENT_SERVER
+  // when `next build` or `npm run build` is used
+  const isProd = phase === PHASE_PRODUCTION_BUILD && process.env.STAGING !== '1'
+  // when `next build` or `npm run build` is used
+  const isStaging =
+    phase === PHASE_PRODUCTION_BUILD && process.env.STAGING === '1'
+
+  console.log(`isDev:${isDev}  isProd:${isProd}   isStaging:${isStaging}`)
+
+  const env = {
+    HOST_KEY: (() => {
+      if (isDev) return 'http://localhost:3000/api'
+      if (isProd) {
+        return 'http://localhost:3000/api'
+      }
+      if (isStaging) return 'http://localhost:3000/api'
+      return 'RESTURL_SPEAKERS:not (isDev,isProd && !isStaging,isProd && isStaging)'
+    })(),
+    HOST_SESSIONS: (() => {
+      if (isDev) return 'http://localhost:3000/api'
+      if (isProd) return 'http://localhost:3000/api'
+      if (isStaging) return 'http://localhost:3000/api'
+      return 'RESTURL_SESSIONS:not (isDev,isProd && !isStaging,isProd && isStaging)'
+    })(),
+  }
+
+  // next.config.js object
+  return {
+    env,
+  }
+}
