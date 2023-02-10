0eaaae40883756c338151a8f3f8a10f24c27a36a
safwan <safwanazman017@gmail.com>
2023-02-10 12:59:11 +0800

    fix api

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,16 @@
 import '@/styles/globals.css'
+import { ThemeProvider } from "next-themes";
+import Navbar from '@/components/navbar';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider enableSystem={true} attribute="class">
+      <Navbar/>
+      <div className="container mx-auto">
+        <div className="px-6">
+          <Component {...pageProps} />
+        </div>
+      </div>
+    </ThemeProvider>
+  );
 }
