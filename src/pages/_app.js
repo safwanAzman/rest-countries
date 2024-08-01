2d55ad9edad82bdf48d1bdedfdf1cab32a71a588
wan <47422904+safwanAzman@users.noreply.github.com>
2024-08-01 21:25:37 +0800

    update

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -14,7 +14,7 @@ export default function App({ Component, pageProps }) {
       <footer className="fixed bottom-0 w-full p-1 bg-black ">
           <div className="flex items-center justify-center text-xs text-white">
             Challenge by <a className="px-1 text-teal-500 border-b-2 border-teal-500" href="https://www.frontendmentor.io/">Frontend Mentor.</a>
-            Coded by <a className="pl-1 text-teal-500 border-b-2 border-teal-500" href="https://portfolio-v2-self.vercel.app/"> SafwanAzman.</a>
+            Coded by <a className="pl-1 text-teal-500 border-b-2 border-teal-500" href="https://www.safwan-azman.dev"> SafwanAzman.</a>
           </div>
       </footer>
     </ThemeProvider>
